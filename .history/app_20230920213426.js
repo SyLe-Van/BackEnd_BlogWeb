77443ee280e02b42d0636e50dd04f382696ed2f2
Sy Le <sycung9001@gmail.com>
2023-09-20 22:01:16 +0700

    first commit

---
+++ .history/app_20230920213426.js
@@ -0,0 +1,16 @@
+const http = require('http')
+const { off } = require('process')
+const port = 3000
+
+const server = http.createServer(function(req, res){
+
+})
+
+server.listen(port, function(error){
+    if(error) {
+        console.log('Something went wrong', error)
+
+    }else {
+        console.log('Service is listening on port' + port)
+    }
+}) 
