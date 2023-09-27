6e4bedb719b774eed6c5a128fd5509d635bd405b
Sy Le <sycung9001@gmail.com>
2023-09-27 16:20:02 +0700

    Template engine

---
+++ src/index.js
@@ -0,0 +1,47 @@
+
+const path = require('path')
+const express = require('express') 
+const cors = require('cors')
+const app = express()
+const morgan = require('morgan')
+
+const handlebars = require('express-handlebars');
+
+
+require('dotenv').config()
+
+
+app.engine('hbs', handlebars.engine({
+    extname: '.hbs'
+}))
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}))
+
+app.use(morgan('combined'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/news', (req, res) => {
+    res.render('news')
+})
+
+
+
+const PORT = process.env.PORT || 8888
+
+const listener =  app.listen(PORT, () => {
+    console.log(`Example app listening at ` + listener.address().port)
+})
+
