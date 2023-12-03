8c3aab3b9f631d1f4666a76dc9cb626d43581fd1
20521811quachvinhquang <hynoras1@gmail.com>
2023-12-03 15:47:00 +0700

    delete comment

--- src/routes/comment.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const commentController = require('../app/controllers/CommentController');
-
-router.get('post/:postid/getComments', commentController.getAllComments)
-
-module.exports = router;
