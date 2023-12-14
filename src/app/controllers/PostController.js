2f54a451ec84caef1c735b5f3742405838cd62ae
20521811quachvinhquang <hynoras1@gmail.com>
2023-12-14 16:30:15 +0700

    debug

--- src/app/controllers/PostController.js
+++ src/app/controllers/PostController.js
@@ -1,9 +1,8 @@
 const fs = require("fs");
 const Post = require("../models/Post");
 const jwt = require("jsonwebtoken");
-// const secret = "uit20521854";
+const secret = "uit20521854";
 const path = require("path");
-const jwtSecret = process.env.JWT_SECRET || "default_secret";
 const uploadSingleFile = async (fileObject) => {
   const { file } = fileObject;
   let uploadPath = path.resolve(__dirname, "../public/images/upload");
@@ -28,24 +27,7 @@ const uploadSingleFile = async (fileObject) => {
     };
   }
 };
-
 class PostController {
-  //[GET] /Post/profile
-  profile(req, res) {
-    const { token } = req.cookies;
-
-    jwt.verify(token, jwtSecret, {}, (err, info) => {
-      if (err) {
-        console.error("Xác thực JWT thất bại:", err.message);
-        return res.status(401).json({ error: "Unauthorized" });
-      }
-      if (!info) {
-        return res.status(401).json({ error: "Unauthorized" });
-      }
-
-      res.json(info);
-    });
-  }
   // [POST] /Post/createPost
   async post(req, res) {
     try {
@@ -61,7 +43,7 @@ class PostController {
       const ext = parts[parts.length - 1];
 
       const { token } = req.cookies;
-      jwt.verify(token, jwtSecret, async (err, info) => {
+      jwt.verify(token, secret, async (err, info) => {
         if (err) throw err;
         const { title, content, categories } = req.body;
         const postDoc = await Post.create({
@@ -131,7 +113,7 @@ class PostController {
       const ext = parts[parts.length - 1];
     }
     const { token } = req.cookies;
-    jwt.verify(token, jwtSecret, {}, async (err, info) => {
+    jwt.verify(token, secret, {}, async (err, info) => {
       if (err) throw err;
       const { id, title, content, categories } = req.body;
       const postDoc = await Post.findById(id);
@@ -140,7 +122,7 @@ class PostController {
       if (!idAuthor) {
         return res.status(403).json("You are not the author of this post");
       }
-      await postDoc.updateOne({
+      await postDoc.update({
         title,
         content,
         categories,
@@ -152,7 +134,6 @@ class PostController {
   // [DELETE] /deletePost/:id
   async deletePost(req, res) {
     const { id } = req.params;
-    
     try {
       await Post.findByIdAndDelete(id);
       res.json({ message: "Post deleted successfully" });
